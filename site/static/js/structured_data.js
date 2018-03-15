42231b8c919a4ddb26c25c85153d99c856d07b06
jonpetersen <jonepetersen@gmail.com>
2018-03-15 19:32:43 +0000

    Structured data

--- site/static/js/structured_data.js
+++ site/static/js/structured_data.js
@@ -10,3 +10,19 @@
         "areaServed": "GB"
       }
 }
+{
+"@context" : "http://schema.org",
+"@type" : "LocalBusiness",
+"name" : "Surrey Hills Cycle Hire",
+"telephone" : "+447506173480",
+"image":"http://d33wubrfki0l68.cloudfront.net/9fcacfbdaf6e1442609e0c318825d6ecea82596b/32c42/img/shch_logo_text.svg",
+"address": {
+"@type": "PostalAddress","streetAddress" : "Pear Tree Cottage","addressLocality" : "Beech Hill","addressRegion" : "Hambledon, Surrey","postalCode" : "GU8 4HL"},
+
+"description" : "We offer bike hire and guided Electric Mountain Bike Tours in the Surrey Hills centred on the beautiful village of Hambledon. ",
+"url" : "https://surreyhillscyclehire.co.uk",
+"logo" : "http://d33wubrfki0l68.cloudfront.net/9fcacfbdaf6e1442609e0c318825d6ecea82596b/32c42/img/shch_logo_text.svg",
+"openingHours": [ "Mo-Su 09:00-17:00" ],
+"sameAs": [ "https://twitter.com/surreyhillsch",
+"https://www.facebook.com/surreyhillscyclehire/" ]
+}
