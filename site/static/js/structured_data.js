7e11d473dd31bdbdca2c23d88c1acdb34bdc8a03
jonpetersen <jonepetersen@gmail.com>
2018-03-15 19:04:18 +0000

    Structured data

---
+++ site/static/js/structured_data.js
@@ -0,0 +1,12 @@
+{
+      "@context": "http://schema.org",
+      "@type": "Organization",
+      "url": "https://surreyhillscyclehire.co.uk",
+      "name": "Surrey Hills Cycle Hire",
+      "contactPoint": {
+        "@type": "ContactPoint",
+        "telephone": "+44-7506173480",
+        "contactType": "reservations",
+        "areaServed": "GB"
+      }
+}
